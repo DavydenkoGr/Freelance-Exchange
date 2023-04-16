9729af961af3e857770d005ae683828724d8bcfc
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-04-16 18:12:43 +0300

    added database

--- FreelanceMarketplace/db/scripts/tasks.sql
+++ FreelanceMarketplace/db/scripts/tasks.sql
@@ -1,9 +1,12 @@
 CREATE TABLE tasks (
 	id long PRIMARY KEY AUTO_INCREMENT,
-	employer_id long FOREIGN KEY REFERENCES employers(id),
-	employee_id long FOREIGN KEY NOT NULL REFERENCES employees(id) ON DELETE cascade,
+	employer_id long,
+	employee_id long NOT NULL,
 	price int NOT NULL,
 	isFree boolean DEFAULT true,
 	isCompleted boolean DEFAULT false,
 	description varchar(10000),
+
+	FOREIGN KEY (employer_id) REFERENCES employers (id),
+	FOREIGN KEY (employee_id) REFERENCES employees (id) ON DELETE cascade,
 );
