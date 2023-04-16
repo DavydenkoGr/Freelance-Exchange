098fa70d3118b1e9382460118635766fed28d196
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-04-17 00:44:06 +0300

    added findById to TaskDao

--- FreelanceMarketplace/src/main/java/org/freelance/DAOs/TaskDao.java
+++ FreelanceMarketplace/src/main/java/org/freelance/DAOs/TaskDao.java
@@ -10,9 +10,23 @@ public class TaskDao extends AbstractHibernateDao<Task> {
         return getCurrentSession().get(Task.class, id);
     }
 
+    @SuppressWarnings("unchecked")
+    public List<Task> findByEmployerId(long id) {
+        return (List<Task>) getCurrentSession().createQuery(
+                "SELECT * FROM tasks WHERE employer_id = " + id
+        ).list();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Task> findByEmployeeId(long id) {
+        return (List<Task>) getCurrentSession().createQuery(
+                "SELECT * FROM tasks WHERE employee_id = " + id
+        ).list();
+    }
+
     @Override
     @SuppressWarnings("unchecked")
     public List<Task> findAll() {
-        return (List<Task>) getCurrentSession().createQuery("FROM tasks").list();
+        return (List<Task>) getCurrentSession().createQuery("SELECT * FROM tasks").list();
     }
 }
