249f31625d22b12172996ca9f2abdf41db15f5f1
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-04-16 14:29:18 +0300

    added reference to tasks.sql

--- FreelanceMarketplace/db/scripts/tasks.sql
+++ FreelanceMarketplace/db/scripts/tasks.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tasks (
 	id long PRIMARY KEY AUTO_INCREMENT,
-	employer_id long,
-	employee_id long NOT null,
+	employer_id long REFERENCES employers(id),
+	employee_id long FOREIGN KEY NOT NULL REFERENCES employees(id) ON DELETE cascade,
 	price int NOT NULL,
 	isFree boolean DEFAULT true,
 	isCompleted boolean DEFAULT false,
