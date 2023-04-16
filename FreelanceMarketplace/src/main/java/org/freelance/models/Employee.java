50183e08ecf5077995cc79c313aa7f4ac828c01c
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-04-16 11:51:18 +0300

    added Employee model

--- FreelanceMarketplace/src/main/java/org/freelance/models/Employee.java
+++ FreelanceMarketplace/src/main/java/org/freelance/models/Employee.java
@@ -1,4 +1,40 @@
 package org.freelance.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.List;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(columnDefinition = "varchar(255) default 'sole trader'")
+    private String organization;
+
+    @Column(length = 10000)
+    private String information;
+
+    @OneToMany(mappedBy = "tasks")
+    private List<Task> tasks;
 }
