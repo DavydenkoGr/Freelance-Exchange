7f6389afb4a0b621c78613d552e43df28629ad8b
Александр Чечеткин <chechetkinsasha@MacBook-Air-Aleksandr-3.local>
2023-04-17 11:58:00 +0300

    Created find/findAll methods

--- FreelanceMarketplace/src/main/java/org/freelance/services/AbstractService.java
+++ FreelanceMarketplace/src/main/java/org/freelance/services/AbstractService.java
@@ -2,8 +2,18 @@ package org.freelance.services;
 
 import org.freelance.DAOs.AbstractHibernateDao;
 
+import java.util.List;
+
 public abstract class AbstractService<T> {
     protected AbstractHibernateDao<T> DAO;
+
+    public T find(long id) {
+        return DAO.find(id);
+    }
+
+    public List<T> findAll() {
+        return DAO.findAll();
+    }
 
     public T create(T entity) {
         return DAO.create(entity);
