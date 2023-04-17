5021866fe97ef0e81ce783d6539930235c57febc
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-04-17 17:04:31 +0300

    added empty AuthenticationController and profileController

--- FreelanceMarketplace/src/main/java/org/freelance/controllers/EmployeeRegistrationController.java
+++
@@ -1,7 +0,0 @@
-package org.freelance.controllers;
-
-import org.freelance.services.EmployeeService;
-
-public class EmployeeRegistrationController extends AbstractRegistrationController<EmployeeService> {
-
-}
--- FreelanceMarketplace/src/main/java/org/freelance/controllers/EmployerRegistrationController.java
+++
@@ -1,7 +0,0 @@
-package org.freelance.controllers;
-
-import org.freelance.services.EmployerService;
-
-public class EmployerRegistrationController extends AbstractRegistrationController<EmployerService> {
-
-}
