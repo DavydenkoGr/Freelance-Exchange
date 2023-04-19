217d2156ff661fe79fd96c78d9d217b575531ace
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-04-19 13:20:15 +0300

    codestyle changes

--- FreelanceExchange/src/main/java/org/freelance/models/Role.java
+++ FreelanceExchange/src/main/java/org/freelance/models/Role.java
@@ -1,6 +1,12 @@
 package org.freelance.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
