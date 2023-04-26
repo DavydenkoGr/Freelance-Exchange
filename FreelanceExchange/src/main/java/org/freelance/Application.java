6ed6a35e96493ed4881ac11898bc786fffd1328c
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-04-26 14:54:10 +0300

    added dependency to pom.xml

--- FreelanceExchange/src/main/java/org/freelance/Application.java
+++ FreelanceExchange/src/main/java/org/freelance/Application.java
@@ -4,11 +4,10 @@ import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SessionFactory sessionFactory = HibernateSessionFactoryUtil.getSessionFactory();
-//        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
