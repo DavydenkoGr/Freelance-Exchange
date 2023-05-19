2cb166694e28051cb039a6573e02ceed11084a0d
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-05-19 19:25:20 +0300

    uncommented spring security

--- FreelanceExchange/src/main/java/org/freelance/configure/CustomUserDetailsService.java
+++ FreelanceExchange/src/main/java/org/freelance/configure/CustomUserDetailsService.java
@@ -1,39 +1,38 @@
-//package org.freelance.configure;
-//
-//import org.freelance.models.User;
-//import org.freelance.services.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//@Service
-//public class CustomUserDetailsService implements UserDetailsService {
-//    @Autowired
-//    private UserService userService;
-//
-//    @Override
-//    @Transactional(readOnly = true)
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User user = userService.find(username);
-//        String role = user.getRole().getName();
-//
-//        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-//
-//        if (role.equals("employer")) {
-//            grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_EMPLOYER"));
-//        } else if (role.equals("employee")) {
-//            grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_EMPLOYEE"));
-//        }
-//
-//        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(),
-//                grantedAuthorities);
-//    }
-//}
+package org.freelance.configure;
+
+import org.freelance.models.User;
+import org.freelance.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.find(username);
+        String role = user.getRole().getName();
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
+        if (role.equals("employer")) {
+            grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_EMPLOYER"));
+        } else if (role.equals("employee")) {
+            grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_EMPLOYEE"));
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(),
+                grantedAuthorities);
+    }
+}
--- FreelanceExchange/src/main/java/org/freelance/configure/SecurityConfigurer.java
+++ FreelanceExchange/src/main/java/org/freelance/configure/SecurityConfigurer.java
@@ -1,38 +1,38 @@
-//package org.freelance.configure;
-//
-//import org.freelance.services.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfigurer {
-//    @Autowired
-//    private CustomUserDetailsService userDetailsService;
-//    @Autowired
-//    private UserService userService;
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .authorizeHttpRequests((authorize) ->
-//                        authorize.requestMatchers("/authentication/**").permitAll()
-//                                .requestMatchers("/profile").permitAll()
-//                                .requestMatchers("tasks/create_task").hasRole("EMPLOYEE")
-//                );
-//        return http.build();
-//    }
-//
-//    @Autowired
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-//    }
-//}
+package org.freelance.configure;
+
+import org.freelance.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurer {
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests((authorize) ->
+                        authorize.requestMatchers("/authentication/**").permitAll()
+                                .requestMatchers("/profile").permitAll()
+                                .requestMatchers("tasks/create_task").hasRole("EMPLOYEE")
+                );
+        return http.build();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+}
