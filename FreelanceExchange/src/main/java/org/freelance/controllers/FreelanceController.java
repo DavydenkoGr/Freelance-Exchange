7f4a1db6e77a906d9211a8e54ccc8d24d7a7c0b0
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-05-20 17:03:06 +0300

    added /freelance page sketch

--- FreelanceExchange/src/main/java/org/freelance/controllers/FreelanceController.java
+++ FreelanceExchange/src/main/java/org/freelance/controllers/FreelanceController.java
@@ -1,13 +1,35 @@
 package org.freelance.controllers;
 
+import org.freelance.models.Task;
+import org.freelance.models.User;
+import org.freelance.services.TaskService;
+import org.freelance.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class FreelanceController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private TaskService taskService;
+
     @RequestMapping("freelance")
     public String freelance(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.find(authentication.getName());
+
+        List<Task> tasks = taskService.findAll();
+
+        model.addAttribute("user", user);
+        model.addAttribute("tasks", tasks);
+
         return "freelance";
     }
 }
