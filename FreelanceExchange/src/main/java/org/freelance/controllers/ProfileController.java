fc9d3c165e08887f2359163b57939b8925f14b4f
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-05-20 11:03:29 +0300

    profile sketch

--- FreelanceExchange/src/main/java/org/freelance/controllers/ProfileController.java
+++ FreelanceExchange/src/main/java/org/freelance/controllers/ProfileController.java
@@ -1,15 +1,30 @@
 package org.freelance.controllers;
 
 import org.freelance.models.User;
+import org.freelance.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class ProfileController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("profile")
     public String profile(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.find(authentication.getName());
+
+        if (user == null) {
+            return "redirect:/authentication/login";
+        }
+
+        model.addAttribute("user", user);
+
         return "profile";
     }
 }
