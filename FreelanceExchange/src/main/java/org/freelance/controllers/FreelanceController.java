b37b8966cdae217e3908ef03e5bbb5faad9ce519
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-06-01 19:16:17 +0300

    realized task search

--- FreelanceExchange/src/main/java/org/freelance/controllers/FreelanceController.java
+++ FreelanceExchange/src/main/java/org/freelance/controllers/FreelanceController.java
@@ -9,9 +9,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class FreelanceController {
@@ -20,12 +20,19 @@ public class FreelanceController {
     @Autowired
     private TaskService taskService;
 
-    @RequestMapping("freelance")
-    public String freelance(Model model) {
+    @GetMapping("freelance")
+    public String freelance(Model model, String request) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.find(authentication.getName());
+        System.out.println(request);
 
         List<Task> tasks = taskService.findFree();
+
+        if (request != null) {
+            tasks = tasks.stream()
+                    .filter(task -> task.getName().toLowerCase().contains(request.toLowerCase()))
+                    .collect(Collectors.toList());
+        }
 
         model.addAttribute("user", user);
         model.addAttribute("tasks", tasks);
