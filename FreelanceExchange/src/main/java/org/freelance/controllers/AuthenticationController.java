0c33fdadf8e4e50adf0b38851dbfd75860a7e40b
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-06-02 09:55:19 +0300

    added AuthenticationController javadoc

--- FreelanceExchange/src/main/java/org/freelance/controllers/AuthenticationController.java
+++ FreelanceExchange/src/main/java/org/freelance/controllers/AuthenticationController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Login and register controller
+ */
 @Controller
 @RequestMapping("authentication")
 public class AuthenticationController {
@@ -25,11 +28,20 @@ public class AuthenticationController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    /**
+     * Login page handler
+     */
     @GetMapping("login")
     public String login() {
         return "login-form";
     }
 
+    /**
+     * Employer registration page handler
+     * Contain registration form for user
+     * Call /authentication/registration/save page when user completes form
+     * @param model transfers data to templates
+     */
     @GetMapping("employer/registration")
     public String employerRegistration(Model model) {
         RegistrationForm form = new RegistrationForm();
@@ -39,6 +51,12 @@ public class AuthenticationController {
         return "employer-registration-form";
     }
 
+    /**
+     * Employee registration page handler
+     * Contain registration form for user
+     * Call /authentication/registration/save page when user completes form
+     * @param model transfers data to templates
+     */
     @GetMapping("employee/registration")
     public String employeeRegistration(Model model) {
         RegistrationForm form = new RegistrationForm();
@@ -48,6 +66,11 @@ public class AuthenticationController {
         return "employee-registration-form";
     }
 
+    /**
+     * Registration form handler, check if form valid and create new user
+     * @param form valid registration form
+     * @param result processing report
+     */
     @PostMapping("registration/save")
     public String registrationSave(@Valid @ModelAttribute("form") RegistrationForm form, BindingResult result) {
         User existingUser = userService.find(form.getLogin());
