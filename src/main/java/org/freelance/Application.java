7317427516cfa1629c4adf2c3d2df2e98ab676b4
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-06-08 01:17:06 +0300

    merged auto roles creation

--- src/main/java/org/freelance/Application.java
+++ src/main/java/org/freelance/Application.java
@@ -1,5 +1,7 @@
 package org.freelance;
 
+import org.freelance.models.Role;
+import org.freelance.services.RoleService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,11 +12,36 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Application {
     /**
+     * Default role creation method.
+     * Create "employer" and "employee" roles
+     *
+     * @param roleService default RoleService
+     */
+    private static void createRoles(RoleService roleService) {
+        Role employerRole = new Role();
+        employerRole.setName("employer");
+        employerRole.setId(1);
+
+        Role employeeRole = new Role();
+        employeeRole.setName("employee");
+        employeeRole.setId(2);
+
+        roleService.create(employerRole);
+        roleService.create(employeeRole);
+    }
+
+    /**
      * Main method
      * Run SpringBootApplication
      * @param args command line args
      */
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        RoleService roleService = context.getBean(RoleService.class);
+
+        if (roleService.findByName("employer") == null || roleService.findByName("employee") == null) {
+            createRoles(roleService);
+        }
     }
 }
